b57f4772d5bac6d22c14e149255acf4fd2d59b9d
Arm1700 <bukboks1@gmail.com>
2025-10-27 01:01:26 +0400

    feat: accounts

--- prisma/migrations/20251026170524_init/migration.sql
+++ prisma/migrations/20251026170524_init/migration.sql
@@ -4,9 +4,6 @@ CREATE EXTENSION IF NOT EXISTS "pg_trgm";
 -- CreateExtension
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
--- CreateExtension
-CREATE EXTENSION IF NOT EXISTS "uuid_ossp";
-
 -- CreateEnum
 CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
 
